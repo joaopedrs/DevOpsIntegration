1241e93506551c716519c50408411604d2c753bd
joaopedrs <82004233+joaopedrs@users.noreply.github.com>
2021-04-20 13:39:07 -0300

    Change message alert to scritp file

--- DevOpsIntegration/Portal/VisConfiguracao.aspx
+++ DevOpsIntegration/Portal/VisConfiguracao.aspx
@@ -3,92 +3,53 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="body" runat="server">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-    <body>
-        <%--<style type="text/css">
-        .messagealert {
-            border:1px solid black;
-/*
-            width: 100%;
-            position: fixed;
-             top:0px;
-            z-index: 100000;
-            padding: 0;
-            font-size: 15px;*/
-        }
-    </style>
-         <script type="text/javascript">
-             function ShowMessage(message, messagetype) {
-                 var cssclass;
-                 switch (messagetype) {
-                     case 'Success':
-                         cssclass = 'alert-success'
-                         break;
-                     case 'Error':
-                         cssclass = 'alert-danger'
-                         break;
-                     case 'Warning':
-                         cssclass = 'alert-warning'
-                         break;
-                     default:
-                         cssclass = 'alert-info'
-                 }
-                 $('.divForm #alert_container').append('<div id="alert_div" style="margin: 0 0.5%; -webkit-box-shadow: 3px 4px 6px #999;" class="alert ' + cssclass + ' fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>' + messagetype + '!</strong> <span>' + message + '</span></div>');
+	<html xmlns="http://www.w3.org/1999/xhtml">
+	<body>
+		<div class="divForm">
+			<form id="form" runat="server">
+				<div class="container-fluid">
+					<div class="row">
+						<div class="col-2">
+							<div class="d-inline-block">
+								<div class="list-group" id="minhaLista" role="tablist">
+									<a class="list-group-item list-group-item-action active" data-toggle="list" href="#autorizacao" role="tab">Autorização</a>
+									<a class="list-group-item list-group-item-action" data-toggle="list" href="#sprint" role="tab">Sprint</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-9">
+							<div class="tab-content">
+								<div class="tab-pane active" id="autorizacao" role="tabpanel">
+									<asp:Label runat="server" ID="lbURL" AssociatedControlID="txtURL">DevOps URL:</asp:Label>
+									<asp:TextBox runat="server" ID="txtURL" CssClass="form-control" TextMode="Url"></asp:TextBox>
+									<asp:Label runat="server" ID="lbAccessToken" AssociatedControlID="txtAccessToken">Access Token:</asp:Label>
+									<asp:TextBox runat="server" ID="txtAccessToken" CssClass="form-control"></asp:TextBox>
+								</div>
+								<div class="tab-pane" id="sprint" role="tabpanel">
+									<div>
+										<asp:Label runat="server" ID="lbSprintAtiva" AssociatedControlID="ddlSprintAtiva">Sprint Ativa:</asp:Label>
+										<asp:DropDownList runat="server" CssClass="form-select" ID="ddlSprintAtiva">
+										</asp:DropDownList>
 
-                 setTimeout(function () {
-                     $("#alert_div").fadeTo(4000, 500).slideUp(500, function () {
-                         $("#alert_div").remove();
-                     });
-                 }, 500);//500=0,5 seconds to open
-             }
-         </script>--%>
-        
-        <div class="divForm">
-            <%--<div class="messagealert" id="alert_container">teste
-            </div>--%>
-            <form id="form" runat="server">
-                <div class="row">
-                    <div class="col-2">
-                        <div class="d-inline-block">
-                            <div class="list-group" id="minhaLista" role="tablist">
-                                <a class="list-group-item list-group-item-action active" data-toggle="list" href="#autorizacao" role="tab">Autorização</a>
-                                <a class="list-group-item list-group-item-action" data-toggle="list" href="#sprint" role="tab">Sprint</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-9">
-                        <div class="tab-content">
-                            <div class="tab-pane active" id="autorizacao" role="tabpanel">
-                                <asp:Label runat="server" ID="lbURL" AssociatedControlID="txtURL">DevOps URL:</asp:Label>
-                                <asp:TextBox runat="server" ID="txtURL" CssClass="form-control" TextMode="Url"></asp:TextBox>
-                                <asp:Label runat="server" ID="lbAccessToken" AssociatedControlID="txtAccessToken">Access Token:</asp:Label>
-                                <asp:TextBox runat="server" ID="txtAccessToken" CssClass="form-control"></asp:TextBox>
-                            </div>
-                            <div class="tab-pane" id="sprint" role="tabpanel">
-                                <div>
-                                    <asp:Label runat="server" ID="lbSprintAtiva" AssociatedControlID="ddlSprintAtiva">Sprint Ativa:</asp:Label>
-                                    <asp:DropDownList runat="server" CssClass="form-select" ID="ddlSprintAtiva">
-                                    </asp:DropDownList>
-
-                                    <asp:CheckBox runat="server" CssClass="form-check-label" ID="chkSprintAtiva" Checked="true" />
-                                    <asp:Label runat="server" ID="lbchkSprintAtiva" AssociatedControlID="chkSprintAtiva">Buscar Sprint Automaticamente</asp:Label>
-                                </div>
-                                <div>
-                                    <asp:Label runat="server" ID="lbWorkItem" AssociatedControlID="txtWorkItem">Principal WorkItem:</asp:Label>
-                                    <img src="../Estilos/Icones/info.png" width="15" height="15" data-toggle="tooltip" data-placement="top" title="(Descrição) WorkItem onde serão adicionado as tasks." />
-                                    <asp:TextBox runat="server" ID="txtWorkItem" CssClass="form-control"></asp:TextBox>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="divForm">
-                            <asp:Button Text="Salvar" runat="server" ID="btnSalvar" OnClick="btnSalvar_Click" CssClass="btn btn-primary mb-3" />
-                        </div>
-
-                    </div>
-                </div>
-                <div class="col-1"></div>
-            </form>
-        </div>
-    </body>
-    </html>
+										<asp:CheckBox runat="server" CssClass="form-check-label" ID="chkSprintAtiva" Checked="true" />
+										<asp:Label runat="server" ID="lbchkSprintAtiva" AssociatedControlID="chkSprintAtiva">Buscar Sprint Automaticamente</asp:Label>
+									</div>
+									<div>
+										<asp:Label runat="server" ID="lbWorkItem" AssociatedControlID="txtWorkItem">Principal WorkItem:</asp:Label>
+										<img src="../Estilos/Icones/info.png" width="15" height="15" data-toggle="tooltip" data-placement="top" title="(Descrição) WorkItem onde serão adicionado as tasks." />
+										<asp:TextBox runat="server" ID="txtWorkItem" CssClass="form-control"></asp:TextBox>
+									</div>
+								</div>
+							</div>
+							<div class="divForm">
+								<asp:Button Text="Salvar" runat="server" ID="btnSalvar" OnClick="btnSalvar_Click" CssClass="btn btn-primary mb-3" />
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="messagealert" id="alert_container"></div>
+			</form>
+		</div>
+	</body>
+	</html>
 </asp:Content>
--- DevOpsIntegration/Portal/VisConfiguracao.aspx.designer.cs
+++ DevOpsIntegration/Portal/VisConfiguracao.aspx.designer.cs
@@ -11,115 +11,115 @@ namespace DevOpsIntegration.Portal
 {
 
 
-    public partial class VisConfiguracao
-    {
+	public partial class VisConfiguracao
+	{
 
-        /// <summary>
-        /// Controle form.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form;
+		/// <summary>
+		/// Controle form.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.HtmlControls.HtmlForm form;
 
-        /// <summary>
-        /// Controle lbURL.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbURL;
+		/// <summary>
+		/// Controle lbURL.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lbURL;
 
-        /// <summary>
-        /// Controle txtURL.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtURL;
+		/// <summary>
+		/// Controle txtURL.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtURL;
 
-        /// <summary>
-        /// Controle lbAccessToken.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbAccessToken;
+		/// <summary>
+		/// Controle lbAccessToken.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lbAccessToken;
 
-        /// <summary>
-        /// Controle txtAccessToken.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtAccessToken;
+		/// <summary>
+		/// Controle txtAccessToken.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtAccessToken;
 
-        /// <summary>
-        /// Controle lbSprintAtiva.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbSprintAtiva;
+		/// <summary>
+		/// Controle lbSprintAtiva.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lbSprintAtiva;
 
-        /// <summary>
-        /// Controle ddlSprintAtiva.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList ddlSprintAtiva;
+		/// <summary>
+		/// Controle ddlSprintAtiva.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.DropDownList ddlSprintAtiva;
 
-        /// <summary>
-        /// Controle chkSprintAtiva.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.CheckBox chkSprintAtiva;
+		/// <summary>
+		/// Controle chkSprintAtiva.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.CheckBox chkSprintAtiva;
 
-        /// <summary>
-        /// Controle lbchkSprintAtiva.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbchkSprintAtiva;
+		/// <summary>
+		/// Controle lbchkSprintAtiva.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lbchkSprintAtiva;
 
-        /// <summary>
-        /// Controle lbWorkItem.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label lbWorkItem;
+		/// <summary>
+		/// Controle lbWorkItem.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lbWorkItem;
 
-        /// <summary>
-        /// Controle txtWorkItem.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox txtWorkItem;
+		/// <summary>
+		/// Controle txtWorkItem.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtWorkItem;
 
-        /// <summary>
-        /// Controle btnSalvar.
-        /// </summary>
-        /// <remarks>
-        /// Campo gerado automaticamente.
-        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnSalvar;
-    }
+		/// <summary>
+		/// Controle btnSalvar.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnSalvar;
+	}
 }
