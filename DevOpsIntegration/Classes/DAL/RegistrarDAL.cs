d2ba81ba4c4c67590acc574a1cd12ef27ce29c1b
joaopedrs <joaopedrolv52@gmail.com>
2021-04-28 01:10:01 -0300

    Prepare tables and coluns tmp

--- DevOpsIntegration/Classes/DAL/RegistrarDAL.cs
+++ DevOpsIntegration/Classes/DAL/RegistrarDAL.cs
@@ -15,8 +15,8 @@ namespace DevOpsIntegration.Classes.DAL
             ConfiguracaoBLL bllConf = new ConfiguracaoBLL();
             ConfiguracaoInfo infoConf = bllConf.Carregar();
 
-            string sqlInsert = string.Format(@"INSERT INTO ATENDIMENTOS VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', {8}, {9}, '{10}', '{11}', '{12}', '{13}')"
-                                            , info.DsCliente, info.DsAnalista, info.DsPalavraChave, info.DsMotivo, info.DsAcao, info.DsConclusao, info.IdTipoAcesso, null, null, info.IsBug, info.DtInicial, info.DtFinal, null, infoConf.DsSprintAtiva);
+            string sqlInsert = string.Format(@"INSERT INTO REGISTRO (cd_registro, cd_tipo_acesso, cd_analista, ds_cliente, ds_palavrachave, ds_motivo, ds_acao, ds_conclusao, ds_sprint, st_bug, vl_inicial, vl_final) VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}', {8}, {9}, '{10}', '{11}')"
+                                            , info.CdRegistro, info.CdTipoAcesso, info.CdAnalista, info.DsCliente, info.DsPalavraChave, info.DsMotivo, info.DsAcao, info.DsConclusao, info.DsSprint, info.StBug, info.VlInicial, info.VlFinal);
 
             using (SqlConnection cnn = new SqlConnection(infoConf.DsConnectionString))
             {
--- DevOpsIntegration/Classes/Info/RegistrarInfo.cs
+++ DevOpsIntegration/Classes/Info/RegistrarInfo.cs
@@ -7,56 +7,56 @@ namespace DevOpsIntegration.Classes.Info
 {
     public class RegistrarInfo
     {
-        public int IdRegistro { get; set; }
+        public int CdRegistro { get; set; }
+        public int CdTipoAcesso { get; set; }
+        public int CdAnalista { get; set; }
+
         public String DsCliente { get; set; }
-        public int IdTipoAcesso { get; set; }
-        public int IdAnalista { get; set; }
         public String DsPalavraChave { get; set; }
         public String DsMotivo { get; set; }
         public String DsAcao { get; set; }
         public String DsConclusao { get; set; }
-        public bool IsBug { get; set; }
-        public TimeSpan DtInicial { get; set; }
-        public TimeSpan DtFinal { get; set; }
+        public String DsSprint { get; set; }
+
+        public bool StBug { get; set; }
+
+        public TimeSpan? VlInicial { get; set; }
+        public TimeSpan? VlFinal { get; set; }
     }
 }
 
-
-//USE[SuporteTI]
-//GO
-
-///****** Object:  Table [dbo].[ATENDIMENTOS]    Script Date: 27/04/2021 13:15:53 ******/
-//SET ANSI_NULLS ON
-//GO
-
-//SET QUOTED_IDENTIFIER ON
-//GO
-
-//CREATE TABLE [dbo].[REGISTRO](
-
-//[cd_registro][int] IDENTITY(1, 1) NOT NULL,
-
-//[ds_cliente] [varchar](50) NULL,
+//CREATE TABLE [dbo].[REGISTRO] (
+//    [cd_registro] [int] IDENTITY(1, 1) NOT NULL,
 //	[cd_tipo_acesso] [int] NOT NULL,
-
 //    [cd_analista] [int] NOT NULL,
 
-//    [ds_palavrachave] [varchar](300) NOT NULL,
-
-//    [ds_motivo] [varchar](max)NOT NULL,
-//	[ds_acao] [varchar](max)NOT NULL,
-//	[ds_conclusao] [varchar](max)NOT NULL,
-//	[st_bug] [int] NULL DEFAULT 0,
-
-//    [vl_inicial] [varchar](15) NULL,
-//	[vl_final] [varchar](15) NULL,
+//    [ds_cliente] [varchar](50) NULL,
+//    [ds_palavrachave] [varchar](300) NULL,
+//    [ds_motivo] [varchar](max) NOT NULL,
+//	[ds_acao] [varchar](max) NOT NULL,
+//	[ds_conclusao] [varchar](max) NOT NULL,
 //	[ds_sprint] [varchar](15) NOT NULL,
-//PRIMARY KEY CLUSTERED 
-//(
 
-//    [cd_registro] ASC
-//)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON[PRIMARY]
-//) ON[PRIMARY] TEXTIMAGE_ON[PRIMARY]
-//GO
+//    [st_bug] [int] NULL DEFAULT 0,
 
+//    [vl_inicial] [varchar](10) NULL,
+//	[vl_final] [varchar](10) NULL,
 
+//    PRIMARY KEY (cd_registro),
+//    FOREIGN KEY(cd_tipo_acesso) REFERENCES TIPO_ACESSO(cd_tipo_acesso),
+//    FOREIGN KEY(cd_analista) REFERENCES ANALISTA(cd_analista)
+//);
+
+//CREATE TABLE [dbo].[ANALISTA] (
+//    [cd_analista] [int] IDENTITY(1, 1) NOT NULL,
+//	[ds_analista] [varchar](30) NOT NULL,
+
+//    PRIMARY KEY (cd_analista)
+//);
+
+//CREATE TABLE [dbo].[TIPO_ACESSO] (
+//    [cd_tipo_acesso] [int] IDENTITY(1, 1) NOT NULL,
+//	[ds_tipo_acesso] [varchar](30) NOT NULL,
+
+//    PRIMARY KEY (cd_tipo_acesso)
+//);
